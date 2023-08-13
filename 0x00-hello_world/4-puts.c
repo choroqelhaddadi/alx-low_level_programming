71ffae04244afaa2ee480fb9f85b9c3d3a32b3f9
choroqelhaddadi <chorouqelhadadi@gmail.com>
2023-08-13 17:15:58 +0300

    C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ /**
+ * main - prints text
+ * 
+ * Return: zero if working correctly
+ */
+int main (void)
+{
+    puts("\"Programming is like building a multilingual puzzle\n");
+    return(0);
+}
+ 
