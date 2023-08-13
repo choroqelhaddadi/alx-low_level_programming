b3428bf1ad0f43fd5ae52a2567cbc21a2c4ac980
choroqelhaddadi <chorouqelhadadi@gmail.com>
2023-08-13 17:23:34 +0300

    C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function main is the entry point of the C program
+ * Return: program should always return 0 when success
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
