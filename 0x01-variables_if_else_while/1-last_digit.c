bb2ed81bdfb44bff3411eaba51d260800d8aa753
choroqelhaddadi <chorouqelhadadi@gmail.com>
2023-08-13 23:33:20 +0300

    C

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - function main is the entry point of the C program
+ * Return: program should always return 0 when success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int last_digit = n % 10;
+
+	if (last_digit == 0)
+		printf("Last digit of %d is %d and is 0", n, last_digit);
+	else if (last_digit > 5)
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_digit);
+	else
+		printf("Last digit of %d is %d and is greater than 5", n, last_digit);
+	return (0);
+}
