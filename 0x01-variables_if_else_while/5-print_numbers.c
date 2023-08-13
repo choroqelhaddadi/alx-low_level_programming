1f50938c1320635e12e5e0cdfed35d2444562ff5
choroqelhaddadi <chorouqelhadadi@gmail.com>
2023-08-14 00:00:35 +0300

    C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - function main is the entry point of the C program
+ * Return: program should always return 0 when success
+ */
+int main(void)
+{
+	char i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
