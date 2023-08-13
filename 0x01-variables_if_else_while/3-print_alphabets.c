99dd1dbe65ebf481c0d521a4fc10342e17cbfb48
choroqelhaddadi <chorouqelhadadi@gmail.com>
2023-08-13 23:55:00 +0300

    C

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,13 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'Z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
-		if (i == 'z')
-			i = 'A' - 1;
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
