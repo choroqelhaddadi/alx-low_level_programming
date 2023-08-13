485da4950489b0306f6419f2407454e338a37a2f
choroqelhaddadi <chorouqelhadadi@gmail.com>
2023-08-13 23:47:03 +0300

    C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - function main is the entry point of the C program
+ * Return: program should always return 0 when success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
