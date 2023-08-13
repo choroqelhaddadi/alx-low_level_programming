6f2e3c088f5928bc71bee4be80e3aacc2b19f144
choroqelhaddadi <chorouqelhadadi@gmail.com>
2023-08-13 17:53:18 +0300

    C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - function main is the entry point of the C program
+ * Return: program should always return 0 when success
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
