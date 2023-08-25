f9d6f98d41dddc6fc85bb9234e19638daed40867
choroqelhaddadi <chorouqelhadadi@gmail.com>
2023-08-25 17:50:40 +0300

    0x06

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,79 +2,80 @@
 #include <stdio.h>
 
 /**
- * remove_left_zeros - removes left zeros of a string.
- * @size: the string size.
+ * rm_zeros - removes left zeros.
+ * @size: string size.
  * @r: string.
- * Return: poniter to string r.
+ * Return: poniter to r.
  */
 
-char *remove_left_zeros(int size, char *r)
+char *rm_zeros(int size, char *r)
 {
-	int i = 0, start_digit;
+	int i = 0, first_digit;
 
 		while (i < size)
 	{
 		if (r[i] != '0')
 		{
-			start_digit = i;
+			first_digit = i;
 			break;
 		}
 		i++;
 	}
 	for (i = 0; i <= size; i++)
 	{
-		r[i] = r[i + start_digit];
+		r[i] = r[i + first_digit];
 	}
 	return (r);
 }
 
 /**
- * infinite_add - a function that adds two numbers.
+ * infinite_add - adds two numbers.
  * @n1: first number.
  * @n2: second number.
  * @size_r: the buffer size.
  * @r: result string.
- * Return: poniter to string if size_r is suitable for the result length.
- * or return 0 if size_r less than the result length.
+ * Return: poniter to r if size_r is acceptable.
+ * or return 0 if size_r less than the length of result.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len_1 = 0, len_2 = 0, len_r = size_r;
-	int m1 = 1, m2 = 1, digit1, digit2, carry = 0, sum = 0;
+	int l1 = 0, l2 = 0;
+	int lresult = size_r;
+	int m1 = 1, m2 = 1, d1, d2, c = 0, sum = 0;
 
-	while (n1[len_1] != '\0')
-		len_1++;
-	while (n2[len_2] != '\0')
-		len_2++;
-	if (len_1 + 1 >= len_r || len_2 + 1 >= len_r)
+	while (n1[l1] != '\0')
+		l1++;
+	while (n2[l2] != '\0')
+		l2++;
+	if (l1 + 1 >= lresult || l2 + 1 >= lresult)
 		return (0);
-	r[len_r] = '\0';
-	len_r--;
-	len_1--;
-	len_2--;
-	while (len_r >= 0)
+	r[lresult] = '\0';
+	lresult--;
+	l1--;
+	l2--;
+	while (lresult >= 0)
 	{
-		digit1 = ((n1[len_1] - '0') * m1);
-		digit2 = ((n2[len_2] - '0') * m2);
-		sum = digit1 + digit2 + carry + '0';
+		d1 = ((n1[l1] - '0') * m1);
+		d2 = ((n2[l2] - '0') * m2);
+		sum = d1 + d2 + c + '0';
 		if (sum > '9')
 		{
 			sum = sum - 10;
-			carry = 1;
+			c = 1;
 		}
 		else
-			carry = 0;
-		r[len_r] = sum;
-		len_r--;
-		if (len_1 <= 0)
+			c = 0;
+		r[lresult] = sum;
+		lresult--;
+		if (l1 <= 0)
 			m1 = 0;
 		else
-			len_1--;
-		if (len_2 <= 0)
+			l1--;
+		if (l2 <= 0)
 			m2 = 0;
 		else
-			len_2--;
+			l2--;
 	}
-	return (remove_left_zeros(size_r, r));
+	return (rm_zeros(size_r, r));
 }
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,58 +2,20 @@
 #include <stdio.h>
 
 /**
- * printLine - prints a buffer
- * @b: pointer to buffer to be printed
- * @size: size of buffer
- * @line: line of buffer to print
- */
-
-void printLine(char *b, int line, int size);
-
-/**
- * print_buffer - prints a buffer
+ * prnt_ln - prints a buffer
  * @b: buffer to print
  * @size: size of buffer
+ * @ln: line of buffer to print
  */
 
-void print_buffer(char *b, int size)
-{
-	int i, len;
-
-	for (i = 0; i <= (size - 1) / 10 && size != 0; i++)
-	{
-		printf("%08x: ", i * 10);
-		if (i < (size / 10))
-		{
-			len = 9;
-			printLine(b, i, len);
-		}
-		else
-		{
-			len = (size % 10) - 1;
-			printLine(b, i, len);
-		}
-		putchar('\n');
-	}
-	if (size == 0)
-		putchar('\n');
-}
-
-/**
- * printLine - prints a buffer
- * @b: buffer to print
- * @size: size of buffer
- * @line: line of buffer to print
- */
-
-void printLine(char *b, int line, int size)
+void prnt_ln(char *b, int ln, int size)
 {
 	int i, j;
 
 	for (j = 0; j <= 9; j++)
 	{
 		if (j <= size)
-			printf("%02x", b[line * 10 + j]);
+			printf("%02x", b[ln * 10 + j]);
 		else
 		{
 			putchar(' ');
@@ -65,9 +27,39 @@ void printLine(char *b, int line, int size)
 	}
 	for (i = 0; i <= size; i++)
 	{
-		if (b[line * 10 + i] <= 31 || b[line * 10 + i] >= 127)
+		if (b[ln * 10 + i] <= 31 || b[ln * 10 + i] >= 127)
 			putchar('.');
 		else
-			putchar(b[line * 10 + i]);
+			putchar(b[ln * 10 + i]);
 	}
+}
+
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, length;
+
+	for (i = 0; i <= (size - 1) / 10 && size != 0; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < (size / 10))
+		{
+			length = 9;
+			prnt_ln(b, i, length);
+		}
+		else
+		{
+			length = (size % 10) - 1;
+			prnt_ln(b, i, length);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
 }
