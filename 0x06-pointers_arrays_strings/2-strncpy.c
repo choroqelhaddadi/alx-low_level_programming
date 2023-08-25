a82151021970b21ce51a2496ed134564682731c4
choroqelhaddadi <chorouqelhadadi@gmail.com>
2023-08-26 01:49:09 +0300

    0x06

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,7 +18,7 @@ char *_strncpy(char *dest, char *src, int n)
 			break;
 		*(dest + length) = *(src + length);
 	}
-	for (length; length < n; length++)
+	for (length; length <= n; length++)
 	{
 		*(dest + length) = '\0';
 	}
