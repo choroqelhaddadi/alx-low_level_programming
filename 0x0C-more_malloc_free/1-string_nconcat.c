2493389aecd72d601b729d61775e4d25d3f88a2b
choroqelhaddadi <chorouqelhadadi@gmail.com>
2023-09-09 02:07:21 +0300

    101

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,58 +1,52 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * string_nconcat - a function that concatenates two strings.
+ * string_nconcat - concatenates two strings.
  *
- * @s1: string 1.
- * @s2: string 2.
- * @n: number of letters used from S2
+ * @s1: first string.
+ * @s2: second string.
+ * @n: number of elements used from S2
  *
- * Return: The returned pointer shall point to
- * a newly allocated space in memory,
- * which contains s1, followed by the first n bytes of s2, and null terminated
- * If the function fails, it should return NULL
- * If n is greater or equal to the length of s2 then use the entire string s2
- * if NULL is passed, treat it as an empty string
+ * Return: pointer to newly allocated space in memory
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0, size;
+	char *p;
+	unsigned int i = 0, j = 0, l1 = 0, l2 = 0, s;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	while (s1 && s1[l1])
+		l1++;
+	while (s2 && s2[l2])
+		l2++;
 
-	if (n >= len2)
+	if (n >= l2)
 	{
-		size = len1 + len2 + 1;
+		s = l1 + l2 + 1;
 	}
 	else
 	{
-		size = len1 + n + 1;
+		s = l1 + n + 1;
 	}
-	ptr = malloc(sizeof(char) * size);
-	if (ptr == NULL)
+	p = malloc(sizeof(char) * s);
+	if (p == NULL)
 		return (NULL);
 
-	while (i < len1)
+	while (i < l1)
 	{
-		ptr[i] = s1[i];
+		p[i] = s1[i];
 		i++;
 	}
 
-	while (n < len2 && i < (len1 + n))
-		ptr[i++] = s2[j++];
+	while (n < l2 && i < (l1 + n))
+		p[i++] = s2[j++];
 
-	while (n >= len2 && i < (len1 + len2))
-		ptr[i++] = s2[j++];
+	while (n >= l2 && i < (l1 + l2))
+		p[i++] = s2[j++];
 
-	ptr[i] = '\0';
+	p[i] = 0;
 
-	return (ptr);
+	return (p);
 }
--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,40 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _realloc - a function that reallocates a memory block
- * using malloc and free
+ * _realloc - reallocates a memory block using malloc and free
  *
  *
- * @ptr: is a pointer to the memory previously allocated with a
- * call to malloc: malloc(old_size).
+ * @ptr: is a pointer to the memory previously allocated
  *
- * @old_size: is the size, in bytes, of the allocated space for ptr.
+ * @old_size: is the size of the allocated space for ptr.
  *
- * @new_size: is the new size, in bytes of the new memory block.
+ * @new_size: is the new size
  * Return: the pointer to the newly created array
- * The contents will be copied to the newly allocated space,
- * in the range from the start of ptr up to the minimum of
- * the old and new sizes
- *
- * If new_size > old_size, the “added” memory should not be initialized
- *
- * If new_size == old_size do not do anything and return ptr
- *
- * If ptr is NULL, then the call is equivalent to malloc(new_size),
- * for all values of old_size and new_size
- * If new_size is equal to zero, and ptr is not NULL, then the call
- * is equivalent to free(ptr). return NULL
- * Don’t forget to free ptr when it makes sense
  *
  *
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int i, size;
-	char *ptrNew, *ptrOld = ptr;
+	int i, s;
+	char *new, *old = ptr;
 
 	if (!ptr)
 		return (malloc(new_size));
@@ -49,21 +33,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (old_size < new_size)
-		size = old_size;
+		s = old_size;
 	else if (old_size > new_size)
-		size = new_size;
+		s = new_size;
 	else
 		return (ptr);
 
-	ptrNew = malloc(new_size);
-	if (!ptrNew)
+	new = malloc(new_size);
+	if (!new)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < s; i++)
 	{
-		*(ptrNew + i) = *(ptrOld + i);
+		*(new + i) = *(old + i);
 	}
 
 	free(ptr);
-	return (ptrNew);
+	return (new);
 }
